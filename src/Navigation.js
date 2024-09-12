f998d0648bed4d1ca30402863528b154eca179ed
Satish-Sk-2004 <satishkumark0809@gmail.com>
2024-09-12 20:06:34 +0530

    Commit

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,22 +1,24 @@
-import React from 'react'
+import React from 'react';
 
 export const Navigation = () => {
   return (
     <div>
       <nav>
         <input type="checkbox" id="check"/>
-          <label for="check" class="checkbtn">
-            <i class="fas fa-bars"></i>
-          </label>
-          <label class="navb-items"><a href="#"><img src="./images/logo.png" class="fclogo" alt="Logo"/></a></label>
-          <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Products</a></li>
-            <li><a href="#">Shop</a></li>
-            <li><a href="#" class="active">Plans</a></li>
-            <li><a href="#">Testimonials</a></li>
-          </ul>
+        <label htmlFor="check" className="checkbtn">
+          <i className="fas fa-bars"></i>
+        </label>
+        <label className="navb-items">
+          <a href="#"><img src="./images/logo.png" className="fclogo" alt="Logo"/></a>
+        </label>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Products</a></li>
+          <li><a href="#">Shop</a></li>
+          <li><a href="#" className="active">Plans</a></li>
+          <li><a href="#">Testimonials</a></li>
+        </ul>
       </nav>
     </div>
-  )
+  );
 }
